8cb82e60ed766ea37fae900c5329f74f6eaa5af5
Vipul Kumar <73470976+vipullsingh@users.noreply.github.com>
2022-11-30 23:31:01 +0530

    Create Palindrome.js

---
+++ Palindrome.js
@@ -0,0 +1,19 @@
+// program to check if the string is palindrome or not
+
+function checkPalindrome(string) {
+
+    // find the length of a string
+    const len = string.length;
+
+    // loop through half of the string
+    for (let i = 0; i < len / 2; i++) {
+
+        //
+
+// take input
+const string = prompt('Enter a string: ');
+
+// call the function
+const value = checkPalindrome(string);
+
+console.log(value);
